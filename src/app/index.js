784abbac91fa78247705fb004c07ed1477deb7e4
neal <anilksee@gmail.com>
2017-09-19 21:49:31 -0400

    First ReactJS component

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,21 @@
-console.log("It works!");
+import React, { Component } from "react";
+import { render } from "react-dom"
+
+class App extends Component {
+
+  render(){
+
+    return(
+        <div className="container">
+          <div className="row">
+            <div className="col-xs-10 col-xs-offset-1">
+              <h1>Hello!</h1>
+            </div>
+          </div>
+
+        </div>
+    );
+  }
+}
+
+render(<App/>, window.document.getElementById("app"));
