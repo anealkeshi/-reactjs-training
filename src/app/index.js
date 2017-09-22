ce169c42a8076a33f31e9b90f39aef3cb6c5cbd7
neal <anilksee@gmail.com>
2017-09-21 21:28:38 -0400

    Passing data from child to parent component again

--- src/app/index.js
+++ src/app/index.js
@@ -6,8 +6,21 @@ import { Home } from "./components/Home";
 
 class App extends Component {
 
+  constructor(props){
+    super();
+    this.state = {
+      homeLink: "Home"
+    };
+  }
+
   onGreet(name){
     alert("Hello " + name);
+  }
+
+  onChangeLinkName(newName) {
+    this.setState({
+      homeLink: newName
+    });
   }
 
   render(){
@@ -16,12 +29,17 @@ class App extends Component {
         <div className="container">
           <div className="row">
             <div className="col-xs-10 col-xs-offset-1">
-              <Header homeLink="Home"/>
+              <Header homeLink={this.state.homeLink}/>
             </div>
           </div>
           <div className="row">
             <div className="col-xs-10 col-xs-offset-1">
-              <Home name={"Max"} initialAge={27} greet={this.onGreet}/>
+              <Home
+                name={"Max"}
+                initialAge={27}
+                greet={this.onGreet}
+                changeLink={this.onChangeLinkName.bind(this)}
+              />
             </div>
           </div>
         </div>
