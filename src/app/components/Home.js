3e5970aa663885eef93136bf036eebc168f754df
neal <anilksee@gmail.com>
2017-09-21 20:58:31 -0400

    Event and state handling

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -2,12 +2,29 @@ import React from "react";
 
 export class Home extends React.Component {
 
+  constructor(props){
+    super();
+    this.state = {
+      age: props.initialAge,
+      status: 0
+    }
+  }
+
+  onMakeOlder(){
+    this.setState({
+      age: this.state.age + 3
+    });
+  }
+
   render(){
-    const age = this.props.age + 1;
         return (
             <div>
-                <h3>Hello {this.props.name}!</h3>
-                <p>You're almost {age}</p>
+                <p>In a new Component</p>
+                <p>You name is {this.props.name}, you age is {this.state.age} </p>
+                <p>Status : {this.state.status} </p>
+                <hr/>
+            {/* <button onClick={this.onMakeOlder.bind(this)} className="btn btn-primary">Make me older!</button> */ }
+                <button onClick={() => this.onMakeOlder()} className="btn btn-primary">Make me older!</button>
             </div>
     );
   }
